c07d1576ec3f83ee911ef0c59c93e2bf6ed3caf8
Gabriel Trindade <icandoitgrt@gmail.com>
2021-11-27 20:17:57 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/projeto_spring/resources/UserResource.java
+++ src/main/java/com/educandoweb/projeto_spring/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.educandoweb.projeto_spring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.projeto_spring.entities.User;
+import com.educandoweb.projeto_spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(21L, "Gabriel", "gabriel.Trindade@gmail.com", "326596565", "Lucian21kjl2");
-		return ResponseEntity.ok(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") //indica que aceita um id dentro da URL
+	public ResponseEntity<User> findById(@PathVariable Long id){ //passa a var (id) para a URL
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
