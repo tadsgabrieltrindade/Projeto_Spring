c6ace0ecd19d0d8b2cdd6fc8a12153ff88e48027
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-04 18:56:17 -0300

    Dev profile PostgreSQL

--- src/main/java/com/educandoweb/projeto_spring/services/UserService.java
+++ src/main/java/com/educandoweb/projeto_spring/services/UserService.java
@@ -3,6 +3,8 @@ package com.educandoweb.projeto_spring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -56,8 +58,7 @@ public class UserService {
 			User entity = repository.getOne(id);
 			updateData(entity, obj);
 			return repository.save(entity);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
